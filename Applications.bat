d5427789f00d20baf245e888c68f057443860f87
techmsafi <techmsafi2@gmail.com>
2025-12-26 12:48:38 +0300

    1st commit

---
+++ Applications.bat
@@ -0,0 +1,27 @@
+@echo off
+
+start "" "https://docs.google.com/document/d/1sd9JY76C4xLUrfLCyQ2GXid5a4p2QhIdGRT8Z-ePkv4/edit?tab=t.0"
+start "" "https://www.fuzu.com/job"
+start "" "https://www.linkedin.com/jobs/"
+start "" "https://www.linkedin.com/company/opened-career/posts/?feedView=all"
+start "" "https://openedcareer.com/category/remote-jobs/"
+start "" "https://campusbiz.co.ke/careers/jobs-in-kenya/"
+start "" "https://www.corporatestaffing.co.ke/category/it-jobs-in-kenya/"
+start "" "https://www.corporatestaffing.co.ke/category/internships-in-kenya/"
+start "" "https://www.corporatestaffing.co.ke/category/graphic-design-jobs-in-kenya/"
+
+FUZU:
+start "" "https://www.fuzu.com/job/computers-software-development?filters[job_id]=760508&filters[country_id]=1&page=1"
+https://www.fuzu.com/job/design-arts?page=1&filters[job_id]=769135
+
+BRIGHTER MONDAY
+start "" "https://www.brightermonday.co.ke/"
+
+
+MYJOBMAG
+start "" "https://www.myjobmag.co.ke/"
+start "" "https://www.myjobmag.co.ke/jobs-by-field/graduate-jobs"
+start "" "https://www.myjobmag.co.ke/jobs-by-field/information-technology"
+start "" "https://www.myjobmag.co.ke/search/jobs?field=Graduate+Jobs"
+
+start "" "https://www.britesmanagement.com/jobs"
